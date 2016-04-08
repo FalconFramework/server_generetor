b12a1ffdb71c8afd214d26de095d84ddc9699681
thiagotmb <tmb0710@gmail.com>
2016-04-07 22:29:26 -0300

    Fixing script for g scaffold

--- falcon_parse.rb
+++ falcon_parse.rb
@@ -13,39 +13,43 @@ class FalconParse
   def create_api
     api_name = @infos['app_name']
     rails_version = "5.0.0.beta3"
-    check_environment_string ='source ~/.rvm/scripts/rvm' +
-    "\n" +
-    'if [[ "$(rvm -v)" != *"1.27"* ]]
+    check_environment_string ='source ~/.rvm/scripts/rvm
+if [[ "$(rvm -v)" != *"1.27"* ]]
 then
   echo "Needs rvm 1.27"
   echo "Installing rvm 1.27..."
   \curl -sSL https://get.rvm.io | bash -s stable --rails ' + "\n " +
-  'echo [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" >> ~/.bash_profile '+ "\n" +
-  'fi' + "\n" +
-'if [[ "$(rvm use ruby-2.3.0)" == *"is not installed"* ]]
+  'echo [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" >> ~/.bash_profile
+fi
+
+if [[ "$(rvm use ruby-2.3.0)" == *"is not installed"* ]]
 then
   echo "Needs ruby 2.3.0"
   echo "Installing ruby 2.3.0..."
   rvm install ruby-2.3.0
-fi' +
-"\nrvm use ruby-2.3.0 \nrvm use ruby-2.3.0@#{api_name} --ruby-version --create \nrvm gemset use #{api_name}
-\n" +
-'if [[ "$(rails -v)" != *"5.0"* ]]
+  rvm use ruby-2.3.0
+fi
+
+if [[ "$(rails -v)" != *"5.0"* ]]
 then
   echo "Needs Rails 5.0"
   echo "Installing Rails 5.0"
-  gem install rails -v' +
-  rails_version +
-  "\nfi \n\n"
+  rvm use ruby-2.3.0@' + "#{api_name}" + ' --ruby-version --create
+  rvm gemset use ' + "#{api_name}" + '
+  gem install rails -v ' + rails_version + '
+fi
 
+'
     @scriptManager.write_to_file(check_environment_string)
     @scriptManager.write_to_file('echo "Creating API..."')
-    @scriptManager.write_to_file("rails _#{rails_version}_ new #{api_name} --api --edge")
-    @scriptManager.write_to_file("cd #{api_name}/")
+    @scriptManager.write_to_file("rails _#{rails_version}_ new ../#{api_name} --api")
+    @scriptManager.write_to_file("cd ../#{api_name}/")
   end
 
   def create_models
     @scriptManager.write_to_file('echo "Creating Models..."')
+    @scriptManager.write_to_file("spring stop")
+
     @models.each do |model, properties|
       current_model_command = "rails g scaffold #{model} "
       properties.each do |property_name, type|
